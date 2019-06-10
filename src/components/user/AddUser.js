19db6d201b63d0d463beb19977fb15d22f9c53e3
pankajthayat <pankajthayat.com>
2019-06-10 07:15:15 +0530

    alingment change

--- src/components/user/AddUser.js
+++ src/components/user/AddUser.js
@@ -2,20 +2,29 @@ import React from "react";
 import { Field, reduxForm } from 'redux-form'
 
 class AddUser extends React.Component {
- 
-  
-  
+  handleOnSubmit = e =>{
+    e.preventDefault();
+    console.log("handle on submit : ", e)
+  }
   render() {
     console.log("props: ", this.props)
     return <div>
-      <form>
+      <form onSubmit = {this.handleOnSubmit}>
+        <div>
      <label>First Name: </label>
      <Field name = "firstName" component = "input"/>
+     </div>
+     <div>
      <label>Last Name: </label>
      <Field name = "lastName" component = "input"/>
+     </div>
+     <div>
      <label>Email: </label>
      <Field name = "email" component = "input"/>
+     </div>
+     <div>
      <button type = "submit">Submit</button>
+     </div>
      </form>
     </div>;
   }
