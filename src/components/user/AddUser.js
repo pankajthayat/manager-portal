020eb35479017bdb85fee4c517377cede4ca253c
pankajthayat <pankajthayat.com>
2019-06-11 07:15:10 +0530

    add user form added

--- src/components/user/AddUser.js
+++ src/components/user/AddUser.js
@@ -1,36 +1,69 @@
 import React from "react";
 import { Field, reduxForm } from 'redux-form'
 
+
+  // this fn is given all the form values and return an errors object
+  const validate =values=>{
+    //errors obj should be in same shape as values obj
+    const errors = {};
+    if(!values.firstName){
+      errors.firstName = 'Required'
+    }
+    if(!values.lastName){
+      errors.lastName = 'Required'
+    }
+    if(!values.email){
+      errors.email = 'Required'
+    }
+    return errors;
+  } 
 class AddUser extends React.Component {
+
+
+
   handleOnSubmit = e =>{
     e.preventDefault();
     console.log("handle on submit : ", e)
   }
+   renderInput =({input,meta, label})=>{
+return(
+  <>
+  <div className="col-sm-3 col-form-label">
+  <label>{label}: </label>
+  </div>
+  <div className="col-sm-9">
+  <input {...input } className="form-control" placeHolder = {label} /> {/* all the meta info are injuected into component here*/ }
+ {meta.error && meta.touched && <span>{meta.error}</span> }
+ </div>
+ </>
+)
+  }
   render() {
     console.log("props: ", this.props)
-    return <div>
-      <form onSubmit = {this.handleOnSubmit}>
-        <div>
-     <label>First Name: </label>
-     <Field name = "firstName" component = "input"/>
+    return <div >
+       <div className="form-component">
+      <form className="form-horizontal" onSubmit = {this.handleOnSubmit}>
+      <div className="container">
+      <div className="row">
+     <Field name = "firstName" label = "First Name" component = {this.renderInput}/> {/* all the meta info are injuected into component here*/ }
      </div>
-     <div>
-     <label>Last Name: </label>
-     <Field name = "lastName" component = "input"/>
+     <div className="row">
+     <Field name = "lastName" label = "Last Name" component = {this.renderInput}/> {/* all the meta info are injuected into component here*/ }
      </div>
-     <div>
-     <label>Email: </label>
-     <Field name = "email" component = "input"/>
+     <div className="row">
+     <Field name = "email" label = "email" component = {this.renderInput}/> {/* all the meta info are injuected into component here*/ }
      </div>
-     <div>
      <button type = "submit">Submit</button>
      </div>
      </form>
+     </div>
     </div>;
   }
+
 }
 
-
 export default reduxForm({
-  form: "user"
+  form: "user",
+  destroyOnUnmount: false, //to not destroy from data on unmount
+  validate: validate
 })(AddUser)
